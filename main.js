56d5e102d57e14af0f66e9a666a8263201d84499
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-06-23 18:58:16 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -6,18 +6,23 @@ function setup() {
     canvas = createCanvas(640, 480);
     canvas.position(110, 250);
     video = createCapture(VIDEO);
+    video.center();
     video.hide();
 }
 
 function draw() {
-    image(video, 320, 240, 200, 200);
+    image(video, 350, 250, 200, 200);
     fill(255, 0, 0);
-    circle(75, 75, 100, 100, 30);
-    circle(75, 400, 100, 100, 30);
-    circle(550, 75, 100, 100, 30);
-    circle(550, 400, 100, 100, 30);
+    circle(75, 75, 40);
+    circle(75, 400, 40);
+    circle(550, 75, 40);
+    circle(550, 400, 40);
     fill(0, 255, 0);
-    rect(175, 250, 400, 30);
+    rect(95, 60, 435, 30);
+    rect(95, 385, 435, 30);
+    rect(60, 95, 30, 285);
+    rect(535, 95, 30, 285);
+    
 
 }
 
