2cd72b81f6aeb1e03101dc28563f9411f7e4da89
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-06-23 16:47:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,11 @@ function setup() {
     canvas = createCanvas(640, 480);
     canvas.position(110, 250);
     video = createCapture(VIDEO);
-    video.center();
     video.hide();
 }
 
 function draw() {
-    image(video, 350, 250, 200, 200);
+    image(video, 320, 240, 200, 200);
     fill(255, 0, 0);
     circle(75, 75, 100, 100, 30);
     circle(75, 400, 100, 100, 30);
